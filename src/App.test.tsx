3cb5cf649c2dba53909056fbcacd76cd54909531
Kisakye Paul <116822457+PaulDeveloper73@users.noreply.github.com>
2026-02-13 12:58:13 +0300

    chore: format code with Prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("App Component", () => {
-  it("renders portfolio name", () => {
+describe('App Component', () => {
+  it('renders portfolio name', () => {
     render(<App />);
     expect(screen.getByText(/Kisakye Paul/i)).toBeInTheDocument();
   });
--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,10 @@ export default function App() {
     <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
       <div className="bg-slate-800 p-10 rounded-2xl shadow-xl text-center">
         <h1 className="text-4xl font-bold text-sky-400">
-          <span>
-
-          Kisakye Paul -
-          </span>
-          <span className="text-slate-100 text-sm">
-            Software Developer
-          </span>
+          <span>Kisakye Paul -</span>
+          <span className="text-slate-100 text-sm">Software Developer</span>
         </h1>
-        <p className="mt-4 text-slate-300">
-          React 19 + Tailwind v4 is working 🚀
-        </p>
+        <p className="mt-4 text-slate-300">React 19 + Tailwind v4 is working 🚀</p>
       </div>
     </div>
   );
