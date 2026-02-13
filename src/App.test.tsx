fdd0be9b0437596970231d0034416e0b094ee5c4
PaulDeveloper73 <PaulDeveloper73@users.noreply.github.com>
2026-02-13 07:40:21 +0000

    deploy: a33b99a6cf36d8c52a5b93708f5e1831d36f3d23

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-describe("App Component", () => {
-  it("renders portfolio name", () => {
-    render(<App />);
-    expect(screen.getByText(/Kisakye Paul/i)).toBeInTheDocument();
-  });
-});
