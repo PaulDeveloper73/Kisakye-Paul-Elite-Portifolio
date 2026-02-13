f24c466ed32651dbfee12676882598b87a9e5d0a
Kisakye Paul <116822457+PaulDeveloper73@users.noreply.github.com>
2026-02-13 10:03:52 +0300

    Added the vitest test code

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App Component", () => {
+  it("renders portfolio name", () => {
+    render(<App />);
+    expect(screen.getByText(/Kisakye Paul/i)).toBeInTheDocument();
+  });
+});
