eb663a1f0f95968d1eb0ae331d8201dd7bfa81f2
Kisakye Paul <116822457+PaulDeveloper73@users.noreply.github.com>
2026-02-13 10:51:39 +0300

    Added the developer role

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ export default function App() {
     <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
       <div className="bg-slate-800 p-10 rounded-2xl shadow-xl text-center">
         <h1 className="text-4xl font-bold text-sky-400">
-          Kisakye Paul
+          Kisakye Paul -
+          <span>
+            Software Developer
+          </span>
         </h1>
         <p className="mt-4 text-slate-300">
           React 19 + Tailwind v4 is working 🚀
