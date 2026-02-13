8e3bf178b8dbafc6c6b3526a7f337fcc9dff5701
Kisakye Paul <116822457+PaulDeveloper73@users.noreply.github.com>
2026-02-13 10:53:56 +0300

    Added the role font size

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ export default function App() {
     <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
       <div className="bg-slate-800 p-10 rounded-2xl shadow-xl text-center">
         <h1 className="text-4xl font-bold text-sky-400">
-          Kisakye Paul -
           <span>
+
+          Kisakye Paul -
+          </span>
+          <span className="text-slate-100 text-sm">
             Software Developer
           </span>
         </h1>
